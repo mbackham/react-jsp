155bba90bdfda690d30ff6c7356b5f0af857ba94
beckhamabo <634264926@qq.com>
2020-07-15 17:57:46 +0800

    input

--- src/Xjj.js
+++ src/Xjj.js
@@ -1,10 +1,24 @@
 import React,{Component,Fragment} from 'react'
 class Xiaojiejie extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            inputVal:'',
+            list:[]
+        }
+
+    }
+     changeVal=(e)=>{
+        console.log(e.target.value)
+        this.setState({
+            inputVal:e.target.value
+        })
+    }
     render(){
         return(
            <Fragment>
             <div>
-                <input/><button>增加选项</button>
+                <input value={this.state.inputVal} onChange={this.changeVal}/><button>增加选项</button>
             </div>
             <ul><li>头部</li><li>头部</li></ul>
         </Fragment>
