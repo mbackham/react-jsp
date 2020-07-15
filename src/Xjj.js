71903d0b48ac86cc428a3eb57b1a49680442293d
beckhamabo <634264926@qq.com>
2020-07-15 17:18:19 +0800

    second commit

---
+++ src/Xjj.js
@@ -0,0 +1,11 @@
+import React,{Component,Fragment} from 'react'
+class Xiaojiejie extends Component{
+    render(){
+        <Fragment>
+            <div>
+                <input/><button>增加选项</button>
+            </div>
+            <ul><li>头部</li><li>头部</li></ul>
+        </Fragment>
+    }
+}
