03b89b3ac200d8bb40eed6554e1cb559786b6ad0
beckhamabo <634264926@qq.com>
2020-07-16 18:02:18 +0800

    踩坑集合

--- src/Xjj.js
+++ src/Xjj.js
@@ -1,29 +1,55 @@
-import React,{Component,Fragment} from 'react'
-class Xiaojiejie extends Component{
-    constructor(props){
+import React, { Component, Fragment } from 'react'
+class Xiaojiejie extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            inputVal:'',
-            list:[]
+        this.state = {
+            inputVal: '',
+            list: ["劳务管理", "机械设备"]
         }
-
     }
-     changeVal=(e)=>{
+    changeVal = (e) => {
         console.log(e.target.value)
         this.setState({
-            inputVal:e.target.value
+            inputVal: e.target.value
         })
     }
-    render(){
-        return(
-           <Fragment>
-            <div>
-                <input value={this.state.inputVal} onChange={this.changeVal}/><button>增加选项</button>
-            </div>
-            <ul><li>头部</li><li>头部</li></ul>
-        </Fragment>
+    addList = () => {
+        this.setState({
+            list: [...this.state.list, this.state.inputVal],
+            inputVal: ''
+        })
+    }
+    deleteVal = (e) => {
+        let list = this.state.list//先声明变量
+        list.splice(e, 1)//再操作变量 （不能直接操作this.state删减）
+        this.setState({
+            list: list
+        }
         )
+    }
+    render() {
+        return (
+            <Fragment>
 
+                <div>
+                    <label htmlFor='koma'>增加项目：</label>
+
+                    <input id='koma' value={this.state.inputVal} onChange={this.changeVal} /><button onClick={this.addList}>增加选项</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li
+                                key={index + item}
+                                onClick={this.deleteVal.bind(this,index)}
+                                dangerouslySetInnerHTML={{__html:item}} ></li>
+                            )
+                        })
+                    }
+                </ul>
+            </Fragment>
+        )
     }
 }
 export default Xiaojiejie
---
+++ src/test.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Test extends Component {
+    state = {  }
+    render() {
+        return (<div>项目</div>);
+    }
+}
+
+export default Test;
